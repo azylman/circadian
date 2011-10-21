346b12acc7efa29c81a7660244fd5213010578d7
Alex Zylman <alex.zylman@gmail.com>
2011-10-20 22:25:25 -0500

    initial commit

---
+++ Me/src/com/zylman/alex/MeServlet.java
@@ -0,0 +1,11 @@
+package com.zylman.alex;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class MeServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
