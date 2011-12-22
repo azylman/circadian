028e8d717df77b67e6853270f20483b9c7d8a71b
Alex Zylman <alex.zylman@gmail.com>
2011-12-21 21:19:38 -0600

    keep track of id

--- src/com/zylman/alex/FeedEntry.java
+++ src/com/zylman/alex/FeedEntry.java
@@ -3,11 +3,13 @@ package com.zylman.alex;
 import java.util.Date;
 
 public class FeedEntry {
+	String id;
 	int source;
 	String content;
 	Date time;
 	
-	FeedEntry(String content, Date time) {
+	FeedEntry(String id, String content, Date time) {
+		this.id = id;
 		this.content = content;
 		this.time = time;
 		this.source = 0;
--- src/com/zylman/alex/TwitterServlet.java
+++ src/com/zylman/alex/TwitterServlet.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.restlet.resource.Get;
 
+import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -12,10 +13,10 @@ public class TwitterServlet extends ServerResource {
 	@Get public String retrieve() {
 		Twitter twitter = HiddenData.getTwitter();
 		try {
-			List<Status> tweets = twitter.getUserTimeline("amzylman");
+			List<Status> tweets = twitter.getUserTimeline("amzylman", new Paging().count(50));
 			Feed feed = new Feed();
 			for (Status tweet : tweets) {
-				feed.add(new FeedEntry(tweet.getText(), tweet.getCreatedAt()));
+				feed.add(new FeedEntry(Long.toString(tweet.getId()), tweet.getText(), tweet.getCreatedAt()));
 			}
 			return feed.toString();
 		} catch (TwitterException e) {
