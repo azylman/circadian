ea66d8472569a95923370e3f3f695dbdc56e1711
Alex Zylman <alex.zylman@gmail.com>
2011-12-22 00:17:44 -0600

    abstract out twitter function

---
+++ src/com/zylman/alex/TwitterHelper.java
@@ -0,0 +1,24 @@
+package com.zylman.alex;
+
+import java.util.List;
+
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class TwitterHelper {
+	public static String get(String user) {
+		Twitter twitter = HiddenData.getTwitter();
+		try {
+			List<Status> tweets = twitter.getUserTimeline(user, new Paging().count(50));
+			Feed feed = new Feed();
+			for (Status tweet : tweets) {
+				feed.add(new FeedEntry(Long.toString(tweet.getId()), tweet.getText(), tweet.getCreatedAt()));
+			}
+			return feed.toString();
+		} catch (TwitterException e) {
+			return "TwitterException: " + e.getMessage();
+		}
+	}
+}
--- src/com/zylman/alex/TwitterResource.java
+++ src/com/zylman/alex/TwitterResource.java
@@ -1,26 +1,9 @@
 package com.zylman.alex;
 
-import java.util.List;
-
 import org.restlet.resource.Get;
-
-import twitter4j.Paging;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
 
 public class TwitterResource extends ServerResource {
 	@Get public String retrieve() {
-		Twitter twitter = HiddenData.getTwitter();
-		try {
-			List<Status> tweets = twitter.getUserTimeline("amzylman", new Paging().count(50));
-			Feed feed = new Feed();
-			for (Status tweet : tweets) {
-				feed.add(new FeedEntry(Long.toString(tweet.getId()), tweet.getText(), tweet.getCreatedAt()));
-			}
-			return feed.toString();
-		} catch (TwitterException e) {
-			return "TwitterException: " + e.getMessage();
-		}
+		return TwitterHelper.get("amzylman");
 	}
 }
