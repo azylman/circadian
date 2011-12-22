e9eb7b4f8559d6831880b29e6e04e00bee64c47f
Alex Zylman <alex.zylman@gmail.com>
2011-12-22 03:15:39 -0600

    cache twitter data and retrieve from cache when possible

--- src/com/zylman/alex/TwitterHelper.java
+++ src/com/zylman/alex/TwitterHelper.java
@@ -1,6 +1,17 @@
 package com.zylman.alex;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+
+import javax.jdo.JDOObjectNotFoundException;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import net.sf.jsr107cache.Cache;
+import net.sf.jsr107cache.CacheException;
+import net.sf.jsr107cache.CacheFactory;
+import net.sf.jsr107cache.CacheManager;
 
 import twitter4j.Paging;
 import twitter4j.Status;
@@ -8,17 +19,89 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
 public class TwitterHelper {
+	private static Cache cache = null;
+	private static final int MAX_PAGES = Integer.MAX_VALUE;
+	
+	public static void instantiateCache() throws CacheException {
+		if (cache == null) {
+			CacheFactory cacheFactory = CacheManager.getInstance().getCacheFactory();
+	        cache = cacheFactory.createCache(Collections.emptyMap());
+		}
+	}
+	
 	public static String get(String user) {
+		String feedData = (String) cache.get(user);
+		
+		if (feedData != null) return feedData;
+		
+		Feed feed = get(user, 1, 20);
+		String feedString = feed.toString();
+		cache.put(user, feedString);
+		if (!feed.isEmpty()) return feedString;
+		
+		return refresh(user);
+	}
+	
+	public static String refresh(String user) {
 		Twitter twitter = HiddenData.getTwitter();
 		try {
-			List<Status> tweets = twitter.getUserTimeline(user, new Paging().count(20));
-			Feed feed = new Feed();
-			for (Status tweet : tweets) {
-				feed.add(new FeedEntry(Long.toString(tweet.getId()), tweet.getText(), tweet.getCreatedAt()));
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+			
+			boolean run = true;
+			int page = 1;
+			while (run) {
+				System.out.println("Getting twitter page " + page);
+				List<Status> tweets = twitter.getUserTimeline(user, new Paging(page, 20));
+				
+				boolean createdObjects = false;
+				for (Status tweet : tweets) {
+					FeedEntry newEntry = new FeedEntry(user, Long.toString(tweet.getId()), tweet.getText(), tweet.getCreatedAt());
+					
+					try {
+						@SuppressWarnings("unused")
+						FeedEntry e = pm.getObjectById(FeedEntry.class, newEntry.tweetId);
+					} catch (JDOObjectNotFoundException e) {
+						pm.makePersistent(newEntry);
+						
+						System.out.println("Creating a feed entry");
+						createdObjects = true;
+					}
+				}
+				
+				if (createdObjects && page++ < MAX_PAGES) {
+					createdObjects = false;
+				} else {
+					run = false;
+				}
 			}
-			return feed.toString();
+			System.out.println("Done fetching tweets");
+			
+			String result = get(user, 1, 20).toString();
+			
+			cache.put(user, result);
+			return result;
 		} catch (TwitterException e) {
 			return "TwitterException: " + e.getMessage();
 		}
+	}
+	
+	private static Feed get(String user, int page, int count) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		
+		Feed feed = new Feed();
+		
+		Query q = pm.newQuery();
+		q.setClass(FeedEntry.class);
+		q.setFilter("user == " + user);
+		q.setOrdering("time descending");
+		q.setRange(page * count, (page + 1) * count);
+		
+		@SuppressWarnings("unchecked")
+		Collection<FeedEntry> entries = (Collection<FeedEntry>) q.execute();
+		for (FeedEntry entry : entries) {
+			feed.add(entry);
+		}
+	
+		return feed;
 	}
 }
--- src/com/zylman/alex/TwitterResource.java
+++ src/com/zylman/alex/TwitterResource.java
@@ -1,9 +1,16 @@
 package com.zylman.alex;
 
+import net.sf.jsr107cache.CacheException;
+
 import org.restlet.resource.Get;
 
 public class TwitterResource extends ServerResource {
 	@Get public String retrieve() {
-		return TwitterHelper.get("amzylman");
+		try {
+			TwitterHelper.instantiateCache();
+			return TwitterHelper.get("amzylman");
+		} catch (CacheException e) {
+			return "CacheException: " + e.getMessage();
+		}
 	}
 }
