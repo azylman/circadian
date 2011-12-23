019b7563dfaaf369519cc67d899f8df2ac7e0e02
Alex Zylman <alex.zylman@gmail.com>
2011-12-23 11:59:47 -0600

    add api support for getting a specific page of tweets

--- src/com/zylman/alex/TwitterResource.java
+++ src/com/zylman/alex/TwitterResource.java
@@ -5,6 +5,8 @@ import org.restlet.resource.Get;
 public class TwitterResource extends ServerResource {
 	@Get public String retrieve() {
 		User user = HiddenData.getAdmin();
-		return TwitterHelper.get(user);
+		String pageNum = getAttribute("pageNum");
+		if (pageNum == null) return TwitterHelper.get(user);
+		return TwitterHelper.get(user, Integer.parseInt(pageNum)).toString();
 	}
 }
