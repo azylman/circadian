18ede950eebba3d9cd3a12d135a1058773c6128a
Alex Zylman <alex.zylman@gmail.com>
2011-12-25 15:32:27 -0600

    return a json representation with an expiration date set to 10 minutes in the future

--- src/com/zylman/alex/ServerResource.java
+++ src/com/zylman/alex/ServerResource.java
@@ -1,11 +1,15 @@
 package com.zylman.alex;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Map;
 
 import org.restlet.data.MediaType;
 import org.restlet.ext.freemarker.TemplateRepresentation;
 import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 
 public class ServerResource extends org.restlet.resource.ServerResource {
 	public String getAttribute(String attr) {
@@ -19,7 +23,20 @@ public class ServerResource extends org.restlet.resource.ServerResource {
         		map,
         		mediaType);
 	}
-	protected Representation populateTemplate(Map<String, Object> map, String templateName) throws IOException {
+	
+	public Representation populateTemplate(Map<String, Object> map, String templateName) throws IOException {
         return toRepresentation(map, templateName, MediaType.TEXT_HTML);
     }
+	
+	public Representation createJSONRepresentation(String text) {
+		Representation result = new StringRepresentation(text, MediaType.APPLICATION_JSON);
+		
+		// Set an expiration date 10 minutes from now.
+		Calendar time = new GregorianCalendar();
+		time.setTime(new Date());
+		time.set(Calendar.MINUTE, time.get(Calendar.MINUTE) + 10);
+		result.setExpirationDate(time.getTime());
+		
+		return result;
+	}
 }
--- src/com/zylman/alex/feed/FeedResource.java
+++ src/com/zylman/alex/feed/FeedResource.java
@@ -1,5 +1,6 @@
 package com.zylman.alex.feed;
 
+import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
 
 import com.zylman.alex.HiddenData;
@@ -7,10 +8,10 @@ import com.zylman.alex.ServerResource;
 import com.zylman.alex.User;
 
 public class FeedResource extends ServerResource {
-	@Get public String retrieve() {
+	@Get public Representation retrieve() {
 		User user = HiddenData.getAdmin();
 		String pageNum = getAttribute("pageNum");
-		if (pageNum == null) return FeedHelper.get(user);
-		return FeedHelper.get(user, Integer.parseInt(pageNum)).toString();
+		if (pageNum == null) return createJSONRepresentation(FeedHelper.get(user));
+		return createJSONRepresentation(FeedHelper.get(user, Integer.parseInt(pageNum)).toString());
 	}
 }
