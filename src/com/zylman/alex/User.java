b89f455e18bb85703073ced6ffb6d1d2ff650910
Alex Zylman <alex.zylman@gmail.com>
2011-12-25 13:36:17 -0600

    add a sources map in our User class

--- src/com/zylman/alex/User.java
+++ src/com/zylman/alex/User.java
@@ -1,5 +1,8 @@
 package com.zylman.alex;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.scribe.model.Token;
 
 public class User {
@@ -7,6 +10,7 @@ public class User {
 	public String twitterID;
 	public String linkedInToken;
 	public String linkedInSecret;
+	private Map<Integer, Source> sources = new HashMap<Integer, Source>();
 	
 	public Token getLinkedInToken() {
 		return new Token(linkedInSecret, linkedInToken);
@@ -18,5 +22,41 @@ public class User {
 	
 	public String getEmail() {
 		return email;
+	}
+	
+	public void addSource(int id, String name, String link) {
+		sources.put(id, new Source(name, link));
+	}
+	
+	public String getSourcesString() {
+		StringBuilder sourceNames = new StringBuilder();
+		StringBuilder sourceLinks = new StringBuilder();
+		
+		sourceNames.append("var sourceName = new Array();\n");
+		sourceLinks.append("var sourceLinks = new Array();\n");
+		for (Map.Entry<Integer, Source> source : sources.entrySet()) {
+			sourceNames.append("sourceName[" + source.getKey() + "] = \"" + source.getValue().getName() + "\";\n");
+			sourceLinks.append("sourceLinks[" + source.getKey() + "] = \"" + source.getValue().getLink() + "\";\n");
+		}
+		
+		return sourceNames.toString() + "\n" + sourceLinks.toString();
+	}
+	
+	public class Source {
+		private String name;
+		private String link;
+		
+		public Source(String name, String link) {
+			this.name = name;
+			this.link = link;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String getLink() {
+			return link;
+		}
 	}
 }
