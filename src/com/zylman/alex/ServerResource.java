c0e5f45f98d64ad15f491352322ba18eb6dd39ba
Alex Zylman <alex.zylman@gmail.com>
2011-12-25 13:35:50 -0600

    add methods to populate a template

--- src/com/zylman/alex/ServerResource.java
+++ src/com/zylman/alex/ServerResource.java
@@ -1,7 +1,25 @@
 package com.zylman.alex;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.restlet.data.MediaType;
+import org.restlet.ext.freemarker.TemplateRepresentation;
+import org.restlet.representation.Representation;
+
 public class ServerResource extends org.restlet.resource.ServerResource {
-	String getAttribute(String attr) {
+	public String getAttribute(String attr) {
 		return (String) getRequest().getAttributes().get(attr);
 	}
+	
+	private Representation toRepresentation(Map<String, Object> map, String templateName, MediaType mediaType) {
+		return new TemplateRepresentation(
+        		templateName,
+        		((MeApplication) getApplication()).getConfiguration(),
+        		map,
+        		mediaType);
+	}
+	protected Representation populateTemplate(Map<String, Object> map, String templateName) throws IOException {
+        return toRepresentation(map, templateName, MediaType.TEXT_HTML);
+    }
 }
